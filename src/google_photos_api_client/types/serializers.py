0af24fcac170b9281e9989875e52fa682e87c7a7
Vinko Mlacic <vinkomlacic@outlook.com>
2021-12-11 02:32:35 +0100

    Add serializer to transform types to JSON dicts

---
+++ src/google_photos_api_client/types/serializers.py
@@ -0,0 +1,19 @@
+__all__ = ['to_dict']
+
+from typing import Sequence
+
+JSON_DATA_TYPES = [str, int, float, None, Sequence, dict]
+
+
+def to_dict(obj):
+    """Creates a dictionary from an object. The dictionary will only have
+    JSON types so it can be easily serialized to JSON string."""
+    data = {}
+
+    for key, value in obj.__dict__.items():
+        if type(value) not in JSON_DATA_TYPES:
+            data[key] = to_dict(value)
+        else:
+            data[key] = value
+
+    return data
