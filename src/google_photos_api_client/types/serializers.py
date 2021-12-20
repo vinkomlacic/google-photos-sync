d36f4f4306932be99af0396a838c40ed17eb00a2
Vinko Mlacic <vinkomlacic@outlook.com>
2021-12-20 22:55:33 +0100

    Add conversion from snake case to camel case when using to_dict function

--- src/google_photos_api_client/types/serializers.py
+++ src/google_photos_api_client/types/serializers.py
@@ -8,8 +8,6 @@ JSON_DATA_TYPES = [str, int, float, None, list, tuple, dict]
 def to_dict(obj):
     """Creates a dictionary from an object. The dictionary will only have
     JSON types so it can be easily serialized to JSON string.
-
-    FIXME: key translation from snake case to camel case is missing
     """
     data = {}
 
@@ -18,14 +16,26 @@ def to_dict(obj):
         if value is None:
             continue
 
+        camel_case_key = snake_case_to_camel_case(key)
+
         # For the primitive types, we just save them
         if type(value) in JSON_DATA_TYPES:
-            data[key] = value
+            data[camel_case_key] = value
         # For enum types, we save the value part in the dict
         elif issubclass(type(value), Enum):
-            data[key] = value.value
+            data[camel_case_key] = value.value
         # We expect that all other types have a __dict__ method
         else:
-            data[key] = to_dict(value)
+            data[camel_case_key] = to_dict(value)
 
     return data
+
+
+def snake_case_to_camel_case(snake_case: str):
+    # Splitting snake case string into words
+    words = snake_case.split('_')
+
+    # Capitalizing every word except the first one and adding the first word at the beginning
+    camel_case = words[0] + ''.join(word.capitalize() for word in words[1:])
+
+    return camel_case
